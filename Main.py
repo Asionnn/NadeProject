685f97f7f1120aa7ba83aac6da79c953202a1fc6
Collin <collin.li0499@gmail.com>
2021-02-04 21:34:35 -0500

    regenerate circles on key press

--- Main.py
+++ Main.py
@@ -3,9 +3,13 @@ import pygame
 import time
 import random
 
+circles = 0
+runOnce = True
+circle_list = []
+black = [0, 0, 0]
+
 
 def euclidean_distance(x1, y1, x2, y2):
-    # return math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
     return math.hypot((x1 - x2), (y1 - y2))
 
 
@@ -15,48 +19,30 @@ def draw_rect_alpha(surface, color, rect):
     surface.blit(shape_surf, rect)
 
 
-if __name__ == "__main__":
-    pygame.init()
-    clock = pygame.time.Clock()
-    window = pygame.display.set_mode([1024, 768])
-    pygame.display.set_caption('SuRT Clone')
-    font50 = pygame.font.SysFont(None, 50)
-    black = [0, 0, 0]
-    window.fill(black)
+def reset_variables():
+    global circles
+    global runOnce
+    global circle_list
 
-    run = True
+    circles = 0
     runOnce = True
     circle_list = []
-    circles = 0
 
-    leftRect = pygame.draw.rect(window, (0, 0, 0), (0, 0, 512, 768), 1)
-    rightRect = pygame.draw.rect(window, (0, 0, 0), (512, 0, 512, 768), 1)
 
-    while run:
+def draw_circles():
+    global circles
+    global runOnce
+
+    while circles <= 50:
         r = 20
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_a:
-                    draw_rect_alpha(window, (100, 100, 100), leftRect)
-                    pygame.display.update()
-                    pygame.time.wait(300)
-                    draw_rect_alpha(window, (0, 0, 0), leftRect)
-                if event.key == pygame.K_d:
-                    draw_rect_alpha(window, (100, 100, 100), rightRect)
-                    pygame.display.update()
-                    pygame.time.wait(300)
-                    draw_rect_alpha(window, (0, 0, 0), rightRect)
-
         x = random.randint(10 + r, window.get_width() - r - 10)
         y = random.randint(10 + r, window.get_height() - r - 10)
 
         if not any((x2, y2, r2) for x2, y2, r2 in circle_list
                    if euclidean_distance(x, y, x2, y2) < r + r2) and circles <= 50:
             circle_list.append((x, y, r))
-            circles = circles + 1
-
+        print(circles)
+        circles = circles + 1
         r = 30
         x = random.randint(10 + r, window.get_width() - r - 10)
         y = random.randint(10 + r, window.get_height() - r - 10)
@@ -68,6 +54,66 @@ if __name__ == "__main__":
         for x, y, r in circle_list:
             pygame.draw.circle(window, (255, 255, 255), (round(x), round(y)), int(r), 1)
         # window.blit(font50.render(str(circles), True, (255, 0, 0)), (10, 10))
+    pygame.display.flip()
+
+
+def highlight_screen(rect):
+    global black
+    draw_rect_alpha(window, (100, 100, 100), rect)
+    pygame.display.update()
+    pygame.time.wait(200)
+    draw_rect_alpha(window, (0, 0, 0), rect)
+    window.fill(black)
+    reset_variables()
+    draw_circles()
+
+
+if __name__ == "__main__":
+    pygame.init()
+    window = pygame.display.set_mode([1024, 768])
+    pygame.display.set_caption('SuRT Clone')
+    font50 = pygame.font.SysFont(None, 50)
+    window.fill(black)
+
+    run = True
+
+    leftRect = pygame.draw.rect(window, (0, 0, 0), (0, 0, 512, 768), 1)
+    rightRect = pygame.draw.rect(window, (0, 0, 0), (512, 0, 512, 768), 1)
+
+    draw_circles()
+
+    while run:
+        r = 20
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            elif event.type == pygame.KEYDOWN:
+                keyPressed = True
+                if event.key == pygame.K_a:
+                    highlight_screen(leftRect)
+                if event.key == pygame.K_d:
+                    highlight_screen(rightRect)
+
+        # x = random.randint(10 + r, window.get_width() - r - 10)
+        # y = random.randint(10 + r, window.get_height() - r - 10)
+        #
+        # if not any((x2, y2, r2) for x2, y2, r2 in circle_list
+        #            if euclidean_distance(x, y, x2, y2) < r + r2) and circles <= 50:
+        #     circle_list.append((x, y, r))
+        #     circles = circles + 1
+        #
+        # r = 30
+        # x = random.randint(10 + r, window.get_width() - r - 10)
+        # y = random.randint(10 + r, window.get_height() - r - 10)
+        # if not any((x2, y2, r2) and not runOnce
+        #            for x2, y2, r2 in circle_list if euclidean_distance(x, y, x2, y2) < r + r2) and runOnce:
+        #     circle_list.append((x, y, r))
+        #     runOnce = False
+        #
+        # for x, y, r in circle_list:
+        #     pygame.draw.circle(window, (255, 255, 255), (round(x), round(y)), int(r), 1)
+        # # window.blit(font50.render(str(circles), True, (255, 0, 0)), (10, 10))
+
         pygame.display.flip()
 
     pygame.quit()
